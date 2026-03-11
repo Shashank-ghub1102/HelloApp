36bc338681e3ac29b8e5fa771b64f0eadce57857
Shashank-ghub1102 <shashankramesh1102@gmail.com>
2026-03-11 11:13:55 +0530

    UC3 updated

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,11 +1,17 @@
 public class HelloApp {
-	public static void main(String[] args) {
-String name = "Shashank";
 
-if (args.length > 0) {
-	name = args[0];
-}
-System.out.println("Hello, " + name);
-	}
-	
+    public static void main(String[] args) {
+
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "Shashank ";
+        }
+
+        System.out.println("Hello, " + name + "!");
+
+    }
+
 }
