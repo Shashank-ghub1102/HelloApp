ed95e36a74638f49cd2fb1bae5c86b6f9c4779b1
Shashank-ghub1102 <shashankramesh1102@gmail.com>
2026-03-11 09:31:27 +0530

    UC2 using command line arguements

--- HelloApp.java
+++ HelloApp.java
@@ -1,11 +1,11 @@
 public class HelloApp {
 	public static void main(String[] args) {
-String name = "World";
+String name = "Shashank";
 
 if (args.length > 0) {
 	name = args[0];
 }
-System.out.println("Hello , " + name);
+System.out.println("Hello, " + name);
 	}
 	
 }
